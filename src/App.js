3e09df7158d0e22bffa768e6a222e1a20de886a4
Imran Khan <imrankhan772244@gmail.com>
2024-07-11 22:43:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <h1>Lets biuld the MERN Tinder clone guys!!!</h1>
       <h1>Lets biuld the MERN Tinder clone guys!!!</h1>
       <h1>Lets biuld the MERN Tinder clone guys!!!</h1>
+      <h1>Lets biuld the MERN Tinder clone guys!!!</h1>
     </div>
   );
 }
