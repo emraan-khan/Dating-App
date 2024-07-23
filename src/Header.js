03b494254eb8c89f2364bdf7389834fdf22d9a89
Imran Khan <imrankhan772244@gmail.com>
2024-07-23 22:13:51 +0530

    update

--- src/Header.js
+++ src/Header.js
@@ -52,6 +52,11 @@ function Header() {
         src={tinderIcon}
         alt=''
     />
+    <img 
+        className='header_logo'
+        src={tinderIcon}
+        alt=''
+    />
     </div>
   )
 }
